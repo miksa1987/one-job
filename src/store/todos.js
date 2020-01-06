91d6635b9e94b98dbfd14bd69e2414b9fbad6ca8
Mika <mika87@gmail.com>
2020-01-06 16:55:51 +0200

    Little outlook tweaking for Intro component

--- src/store/todos.js
+++ src/store/todos.js
@@ -1,48 +1,53 @@
 export default class TodosHandler {
+  todos = [];
+  
   constructor(database) {
     this.database = database;
   }
 
-
-  getAndSetTodos = async (uid) => {
+  getAndSetTodos = async () => {
     return await this.makeTodosObjectToArray();
-  } 
-  
-  setCurrentTodo = (todos, currentDate) => {
-    const filteredTodos = todos.filter((todo) => todo.date === currentDate);
-    let currentTodo = { date: currentDate, task: '', reflect: '' };
-
-    if (this.checkForTodo(currentDate)) {
-      currentTodo = filteredTodos[0];
-    }
-    
-    return currentTodo;
   }
-  
-  checkForTodo = (todos, currentDate) => {
+
+  setCurrentTodo = () => {
+    this.isLoading = true;
+    const filteredTodos = this.todos.filter((todo) => todo.date === this.currentDate);
+    
+    if (this.checkForTodo(this.currentDate)) {
+      this.currentTodo = filteredTodos[0];
+    }
+    else {
+      this.currentTodo = {date: this.currentDate, task: '', reflect: ''};
+    }
+    this.isLoading = false;
+    return this.currentTodo;
+  }
+
+  checkForTodo = () => {
     let found = false;
-    todos.forEach((todo) => {
-      if (todo.date === currentDate) {
+    this.todos.forEach((todo) => {
+      if (todo.date === this.currentDate) {
         found = true;
       }
     });
     
     return found;
   }
-  
-  makeTodosObjectToArray = async (uid) => {
-    const todosObject = await this.database.getTodos(uid);
+
+  makeTodosObjectToArray = async () => {
+    const todosObject = await this.database.getTodos(this.currentUser.uid);
     let todos = [];
     const todosObjectKeys = Object.keys(todosObject);
-  
+
     todosObjectKeys.forEach((key) => {
       todos.push({ ...todosObject[key], key: key });
     })
-  
+
     return todos;
   }
-  
+
   saveTodo = async (todoData, key) => {
     this.database.saveTodo(todoData, key);
   }
+
 }
